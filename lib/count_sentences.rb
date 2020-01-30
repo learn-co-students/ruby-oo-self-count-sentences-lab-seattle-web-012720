646f98e560bf71435f18c50a89cf3458486a2982
Jricecake <jonathanriecke@gmail.com>
2020-01-30 09:51:00 -0800

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[!?.]/).reject{|x| x.empty?}.length
+    # binding.pry
   end
 end
