135c4855572d8bbd0391669aa47350ebae290de1
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-01-30 10:28:42 -0800

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #binding.pry
+    if self.count(".") ==1
+      true
+    else
+      false
+    end
+    #binding.pry
   end
 
   def question?
+    if self.count("?") ==1
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
-
+    if self.count("!") ==1
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    if self.split(/[.!?]/).length == 0
+      return 0
+    else
+     array = self.split(/[.!?]/).reject{|lol|lol==""}
+     array.length
+#binding.pry
   end
+end
 end
